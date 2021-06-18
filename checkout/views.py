55cf15ce554fb145081e2e8986f2ee77ed3a888f
sophnagle <sophnagle@gmail.com>
2021-06-18 19:26:12 +0000

    Add stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
+from django.conf import settings
 
-# Create your views here.
+from .forms import OrderForm
+
+
+def checkout(request):
+    """
+    Checks order form is valid
+    Checks items in bag are valid and in database
+    """
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+
+    bag = request.session.get('bag', {})
+    if not bag:
+        messages.error(request, "There's nothing in your cart at the moment")
+        return redirect(reverse('products'))
+
+    order_form = OrderForm
+    template = 'checkout/checkout.html'
+    context = {
+        'order_form': order_form,
+        'stripe_public_key': stripe_public_key,
+        'client_secret': 'test client secret',
+    }
+
+    return render(request, template, context)
--- crwst/settings.py
+++ crwst/settings.py
@@ -49,6 +49,9 @@ INSTALLED_APPS = [
     'products',
     'bag',
     'checkout',
+
+    # Other
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -62,6 +65,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'crwst.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -78,8 +83,13 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
                 'bag.contexts.bag_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
@@ -164,6 +174,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 FREE_DELIVERY_THRESHOLD = 30
 STANDARD_DELIVERY_PERCENTAGE = 10
+
+# Stripe variables 
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 
 
 # Default primary key field type
