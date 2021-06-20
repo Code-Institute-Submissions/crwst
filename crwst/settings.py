4d408f998c42ce64001ee4a79c286494c9d8cda0
sophnagle <sophnagle@gmail.com>
2021-06-20 07:46:53 +0000

    Add basic checkout functionality

--- crwst/settings.py
+++ crwst/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY', "")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -177,6 +177,8 @@ STANDARD_DELIVERY_PERCENTAGE = 10
 
 # Stripe variables
 STRIPE_PUBLIC_KEY = os.environ.get('STRIPE_PUBLIC_KEY', '')
+STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY', '')
+STRIPE_CURRENCY = 'gbp'
 
 
 # Default primary key field type
