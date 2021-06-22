10f191a66ddfa2cd0618a1924d1b284507c27145
sophnagle <sophnagle@gmail.com>
2021-06-22 20:14:27 +0000

    Delete trailing whitespace

--- crwst/settings.py
+++ crwst/settings.py
@@ -184,7 +184,7 @@ STRIPE_PUBLIC_KEY = os.environ.get('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.environ.get('STRIPE_WH_SECRET', '')
 # Test email account
-DEFAULT_FROM_EMAIL = 'orders@crwst.com' 
+DEFAULT_FROM_EMAIL = 'orders@crwst.com'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
