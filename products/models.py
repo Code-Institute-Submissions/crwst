9eed3488c241bba257a30e8ebd56892c8dbfa4b1
sophnagle <sophnagle@gmail.com>
2021-06-27 15:11:14 +0000

    Add product review model and to product template

--- products/models.py
+++ products/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -26,6 +27,19 @@ class Product(models.Model):
     rating = models.DecimalField(max_digits=6, decimal_places=2)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Review(models.Model):
+    product = models.ForeignKey('Product', null=True, blank=True, on_delete=models.SET_NULL)
+    review_title = models.CharField(max_length=254)
+    review_text = models.TextField()
+    review_rating = models.DecimalField(max_digits=6, decimal_places=2)
+    review_author = models.ForeignKey(User, on_delete=models.CASCADE,
+             related_name="reviews")
+    review_created = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
