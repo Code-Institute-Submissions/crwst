fce2244d6c974a37983190c1488777e7596f9e66
sophnagle <sophnagle@gmail.com>
2021-06-29 17:08:04 +0000

    Remove product review model and template

--- products/models.py
+++ products/models.py
@@ -30,16 +30,3 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
-
-
-class Review(models.Model):
-    product = models.ForeignKey('Product', null=True, blank=True, on_delete=models.SET_NULL)
-    review_title = models.CharField(max_length=254)
-    review_text = models.TextField()
-    review_rating = models.DecimalField(max_digits=6, decimal_places=2)
-    review_author = models.ForeignKey(User, on_delete=models.CASCADE,
-             related_name="reviews")
-    review_created = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return self.name
