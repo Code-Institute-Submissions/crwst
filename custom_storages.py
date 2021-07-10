fca759297c90caa227f23f2097f12e99728063ef
sophnagle <sophnagle@gmail.com>
2021-07-10 12:42:57 +0000

    Amend custom storages syntax

--- custom_storages.py
+++ custom_storages.py
@@ -3,8 +3,8 @@ from storages.backends.s3boto3 import S3Boto3Storage
 
 
 class StaticStorage(S3Boto3Storage):
-    locations: settings.STATICFILES_LOCATION
+    location = settings.STATICFILES_LOCATION
 
 
-class MEDIAStorage(S3Boto3Storage):
-    locations: settings.MEDIAFILES_LOCATION
+class MediaStorage(S3Boto3Storage):
+    location = settings.MEDIAFILES_LOCATION
