8f35261193b7ca98b24d35ab113f9d7cc48d9900
sophnagle <sophnagle@gmail.com>
2021-07-12 10:33:34 +0000

    Increase max length of blog title

--- blog/models.py
+++ blog/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 
 
 class Blog(models.Model):
-    blog_title = models.CharField(max_length=20)
+    blog_title = models.CharField(max_length=60)
     blog = models.TextField()
 
     def __str__(self):
