44ba3022ba5ccf5ef7a0a5c911ac0c621cd07fcf
sophnagle <sophnagle@gmail.com>
2021-07-25 19:19:07 +0000

    Add defense to checkout success so only creator of thar order or superuser can see the confirmation

--- checkout/views.py
+++ checkout/views.py
@@ -147,6 +147,7 @@ def checkout(request):
     return render(request, template, context)
 
 
+@login_required
 def checkout_success(request, order_number):
     """
     Handle successful checkouts
@@ -154,37 +155,43 @@ def checkout_success(request, order_number):
     save_info = request.session.get('save_info')
     order = get_object_or_404(Order, order_number=order_number)
 
-    if request.user.is_authenticated:
-        profile = UserProfile.objects.get(user=request.user)
-        # Attach the user's profile to the order
-        order.user_profile = profile
-        order.save()
+    if order.user_profile.user.username == request.user.username:
 
-        # Save the user's info
-        if save_info:
-            profile_data = {
-                'default_phone_number': order.phone_number,
-                'default_country': order.country,
-                'default_postcode': order.postcode,
-                'default_town_or_city': order.town_or_city,
-                'default_street_address1': order.street_address1,
-                'default_street_address2': order.street_address2,
-                'default_county': order.county,
-            }
-            user_profile_form = UserProfileForm(profile_data, instance=profile)
-            if user_profile_form.is_valid():
-                user_profile_form.save()
+        if request.user.is_authenticated or request.user.is_superuser:
+            profile = UserProfile.objects.get(user=request.user)
+            # Attach the user's profile to the order
+            order.user_profile = profile
+            order.save()
 
-    messages.success(request, f'Order successfully processed. \
-        Your order number is {order_number}. A confirmation \
-            email will be sent to {order.email}.')
+            # Save the user's info
+            if save_info:
+                profile_data = {
+                    'default_phone_number': order.phone_number,
+                    'default_country': order.country,
+                    'default_postcode': order.postcode,
+                    'default_town_or_city': order.town_or_city,
+                    'default_street_address1': order.street_address1,
+                    'default_street_address2': order.street_address2,
+                    'default_county': order.county,
+                }
+                user_profile_form = UserProfileForm(profile_data, instance=profile)
+                if user_profile_form.is_valid():
+                    user_profile_form.save()
 
-    if 'bag' in request.session:
-        del request.session['bag']
+        messages.success(request, f'Order successfully processed. \
+            Your order number is {order_number}. A confirmation \
+                email will be sent to {order.email}.')
 
-    template = 'checkout/checkout_success.html'
-    context = {
-        'order': order,
-    }
+        if 'bag' in request.session:
+            del request.session['bag']
 
-    return render(request, template, context)
+        template = 'checkout/checkout_success.html'
+        context = {
+            'order': order,
+        }
+
+        return render(request, template, context)
+
+    else:
+        messages.error(request, 'You are not authorised to view this page.')
+        return redirect(reverse('home'))
